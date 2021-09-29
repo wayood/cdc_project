35e72f4b9ef88a6b360b79caeb32c6c8849dc1ad
wayood <tomo990203@gmail.com>
2021-09-29 11:06:40 +0900

    publish global plan

--- mighty_rover/src/midleplan_cdc.py
+++ mighty_rover/src/midleplan_cdc.py
@@ -12,39 +12,39 @@ def main():
         rospy.spin()
 
 def callback_con(msg):
-        # i=0
-        # while True:
-        #   print(msg.poses[i].header.stamp)
-        #   if msg.poses[i+1].header.stamp is None:
-        #     break
-        #   i=i+1
-        # print(i)
-        r = rospy.Rate(50)  # 10hz
+        # print(len(msg.poses))
+        # print(msg.poses[len(msg.poses)-1].pose.position.x)
+        r = rospy.Rate(10)  # 10hz
         path_header = msg.poses[0].header
-        path_header.seq = rospy.Time.now()
-        path_header.stamp = 0
+        path_header.seq = 0
+        path_header.stamp = rospy.Time.now()
         path_header.frame_id = "map"
         path = Path()
         path.header = path_header
         pose=cdc_path(msg.poses)
-        path_pub = rospy.Publisher("/path", Path, queue_size=50)
+        path_pub = rospy.Publisher("/move_base/NavfnROS/plan", Path, queue_size=10)
         path.poses=pose
+        print(path)
         path_pub.publish(path)
         r.sleep()    
 
         
   
 def cdc_path(pose):
-    #   if i in range(len(pose.))
-        path=PoseStamped()
-        path.pose.position.x = pose[0].pose.position.x
-        path.pose.position.y = pose[0].pose.position.y
-        path.pose.position.z = pose[0].pose.position.z 
-        path.pose.orientation.x = pose[0].pose.orientation.x
-        path.pose.orientation.y = pose[0].pose.orientation.y
-        path.pose.orientation.z = pose[0].pose.orientation.z
-        path.pose.orientation.w = pose[0].pose.orientation.w
-        return path
+        poses = []
+        for i in range(len(pose)):
+          path=PoseStamped()
+          path.pose.position.x = pose[i].pose.position.x 
+          path.pose.position.y = pose[i].pose.position.y 
+          path.pose.position.z = pose[i].pose.position.z 
+          path.pose.orientation.x = pose[i].pose.orientation.x
+          path.pose.orientation.y = pose[i].pose.orientation.y
+          path.pose.orientation.z = pose[i].pose.orientation.z
+          path.pose.orientation.w = pose[i].pose.orientation.w
+          path.header = pose[i].header
+          path.header.seq = i
+          poses.append(path)
+        return poses
 
 if __name__ == '__main__':
    

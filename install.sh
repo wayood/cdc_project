5f52e8b6bcbf59754b72ab51c56b25dbb6a05b8f
wayood <tomo990203@gmail.com>
2022-09-02 20:54:51 +0900

    インストール用スクリプトの追加

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt install -y ros-${ROS_DISTRO}-joy ros-${ROS_DISTRO}-grid-map ros-${ROS_DISTRO}-teleop-twist-joy ros-${ROS_DISTRO}-teleop-twist-keyboard ros-${ROS_DISTRO}-laser-proc ros-${ROS_DISTRO}-rgbd-launch ros-${ROS_DISTRO}-depthimage-to-laserscan ros-${ROS_DISTRO}-rosserial-arduino ros-${ROS_DISTRO}-rosserial-python ros-${ROS_DISTRO}-rosserial-server ros-${ROS_DISTRO}-rosserial-client ros-${ROS_DISTRO}-rosserial-msgs ros-${ROS_DISTRO}-amcl ros-${ROS_DISTRO}-map-server ros-${ROS_DISTRO}-urdf ros-${ROS_DISTRO}-xacro ros-${ROS_DISTRO}-compressed-image-transport ros-${ROS_DISTRO}-rqt-image-view 
 sudo apt install -y python-opencv ros-${ROS_DISTRO}-find-object-2d ros-${ROS_DISTRO}-gmapping ros-${ROS_DISTRO}-robot-localization ros-${ROS_DISTRO}-urdf-tutorial ros-${ROS_DISTRO}-tf2-sensor-msgs ros-${ROS_DISTRO}-ros-controllers ros-${ROS_DISTRO}-ros-control ros-${ROS_DISTRO}-move-base ros-${ROS_DISTRO}-navigation-msgs ros-${ROS_DISTRO}-costmap-2d
-
+sudo pip3 install rospkg,catkin_pkg
+pip install motpy
 
 
